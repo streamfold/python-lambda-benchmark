e6210c4aee313936b35285da3d119ed9f5cdeb90
Mike Heffner <mikeh@fesnel.com>
2025-04-08 22:02:24 -0400

    Output CSV results

--- scripts/benchmark-coldstart.sh
+++ scripts/benchmark-coldstart.sh
@@ -2,6 +2,14 @@
 
 COUNT=${COUNT:-10}
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <output-file>"
+  exit 1
+fi
+
+OUTPUT="$1"
+shift
+
 if [ -z "$AWS_ROLE_ARN" ]; then
   echo "Must set AWS_ROLE_ARN"
   exit 1
@@ -38,16 +46,16 @@ cd benchmark
 echo "Testing base case"
 
 uv run main.py --path ../function.zip --count $COUNT --function-name benchmark-coldstart \
-  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION"
+  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION" --output "$OUTPUT"
 
-echo "Testing OpenTelmetry collector layer"
+echo "Testing OpenTelemetry collector layer"
 
 uv run main.py --path ../function.zip --count $COUNT --function-name benchmark-coldstart-otel \
   --environment OPENTELEMETRY_COLLECTOR_CONFIG_URI=/var/task/collector.yaml \
-  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION" --layer "$OTEL_LAYER"
+  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION" --layer "$OTEL_LAYER" --output "$OUTPUT"
 
 echo "Testing Rotel layer"
 
 uv run main.py --path ../function.zip --count $COUNT --function-name benchmark-coldstart-rotel \
   --environment ROTEL_ENV_FILE=/var/task/rotel.env \
-  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION" --layer "$ROTEL_LAYER"
+  --role-arn "$AWS_ROLE_ARN" --region "$AWS_REGION" --layer "$ROTEL_LAYER" --output "$OUTPUT"
